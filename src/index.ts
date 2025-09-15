f00b5fb2269ddbf77ba127070d161092dc44dd36
Marcelo <devfullmarcelo@gmail.com>
2025-09-15 16:02:53 -0300

    feito?

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.use(
 
       // Domínios permitidos em produção
       const allowedOrigins = [
-        "https://loja.gofashion.click",
+        "https://gofashion.click",
          // URL do frontend em produção
         "http://localhost:5173",
         "http://localhost:3000",
